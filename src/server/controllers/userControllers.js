f62c947d030c3b4245a512722ebb60f9ee1eb3ae
Inna Iazovkaia <inna.0961@mail.ru>
2022-02-22 14:56:17 +0100

    created userControllers.js

---
+++ src/server/controllers/userControllers.js
@@ -0,0 +1,31 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../../database/models/User");
+
+const userLogin = async (req, res, next) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    const error = new Error("User not found");
+    error.code = 401;
+    next(error);
+  } else {
+    const correctPassword = await bcrypt.compare(password, user.password);
+
+    if (!correctPassword) {
+      const error = new Error("Password not recognized");
+      error.code = 401;
+      next(error);
+    } else {
+      const userDate = {
+        username: user.username,
+        id: user.id,
+      };
+      const token = jwt.sign(userDate, process.env.JWT_SECRET);
+      res.json({ token });
+    }
+  }
+};
+
+module.exports = { userLogin };
